e09f6ff81ff783e351773e3343f5ff8420805786
Nasnast <nasnast@ya.ru>
2023-12-12 15:05:14 +0300

    DZ4

---
+++ Home_04/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 4*(не обязательная): Задайте строку, состоящую 
+из слов, разделенных пробелами. Сформировать строку, 
+в которой слова расположены в обратном порядке. В 
+полученной строке слова должны быть также разделены 
+пробелами. */
+
+namespace Home_04
+{
+    public static class Program
+    {
+        public static void Main(string[] array)
+        {
+            System.Console.WriteLine("введите текст:");
+            string input = System.Console.ReadLine();
+            string[] stroka = input.Split();
+            // int i = 0;
+            // foreach (string str in stroka)
+            // {
+            //     i++;
+            //     System.Console.WriteLine($"Слово {str}");
+            // }
+            // System.Console.WriteLine(i);
+
+            Array.Reverse(stroka);
+
+            for (int j = 0; j < stroka.Length; j++)
+            {
+                System.Console.Write(stroka[j] + " ");
+            }
+
+        }
+
+    }
+
+}
+
+
+
